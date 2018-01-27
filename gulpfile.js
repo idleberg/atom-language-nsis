b096714f9f7fb863f3f3fab6267c98ecabb019d6
idleberg <jan@idleberg.com>
2018-01-27 17:37:31 +0100

    simplify

--- gulpfile.js
+++ gulpfile.js
@@ -23,27 +23,26 @@ const jsonFiles = [
 ];
 
 // Lint CoffeeScript & CSON files
-gulp.task('lint:coffee', gulp.series(function(done) {
+gulp.task('lint:coffee', (done) => {
     gulp.src(coffeeFiles)
         .pipe(debug({title: 'coffeelint:'}))
         .pipe(coffeelint())
         .pipe(coffeelint.reporter(stylish))
         .pipe(coffeelint.reporter('fail'));
     done();
-}));
+});
 
 // Lint JSON files
-gulp.task('lint:json', gulp.series(function(done) {
+gulp.task('lint:json', (done) => {
     gulp.src(jsonFiles)
         .pipe(debug({title: 'jsonlint:'}))
         .pipe(jsonlint())
         .pipe(jsonlint.failAfterError())
         .pipe(jsonlint.reporter());
     done();
-}));
+});
 
 // Tasks
 gulp.task('lint', gulp.parallel('lint:coffee', 'lint:json', function(done) {
   done();
 }));
-
