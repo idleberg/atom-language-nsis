2959da25c1009628f9befd1bb53f6d1ad168ef35
idleberg <git@idleberg.com>
2021-03-11 01:56:19 +0100

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
 /* eslint-disable */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:json/recommended'
+  ],
+  globals: {
+    atom: "readonly"
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
   ],
   rules: {
     // temporary
