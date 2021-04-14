142db74eefe40312f7677e81610b046b76b12914
idleberg <git@idleberg.com>
2021-04-14 21:14:05 +0200

    build

--- src/log.ts
+++ src/log.ts
@@ -1,32 +1,6 @@
-function __console(type: string, ...args: unknown[]): void {
-  if (atom?.inDevMode()) {
-    args.unshift('[language-nsis]')
-    global.console[type](...args);
-  }
-}
+import DeveloperConsole from '@atxm/developer-console';
 
-export default {
-  clear(...args: unknown[]): void {
-    __console('clear', ...args);
-  },
-
-  debug(...args: unknown[]): void {
-    __console('debug', ...args);
-  },
-
-  error(...args: unknown[]): void {
-    __console('error', ...args);
-  },
-
-  info(...args: unknown[]): void {
-    __console('info', ...args);
-  },
-
-  log(...args: unknown[]): void {
-    __console('log', ...args);
-  },
-
-  warn(...args: unknown[]): void {
-    __console('warn', ...args);
-  }
-}
+export default new DeveloperConsole({
+  name: 'language-nsis',
+  backgroundColor: 'coral'
+});
