5deea96fc644266bf919b5c1160d97c0ce5570dc
idleberg <git@idleberg.com>
2021-04-14 21:20:10 +0200

    retrieve package name from manifest

--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,7 @@
+import meta from '../package.json';
 import DeveloperConsole from '@atxm/developer-console';
 
 export default new DeveloperConsole({
-  name: 'language-nsis',
+  name: meta.name,
   backgroundColor: 'coral'
 });
