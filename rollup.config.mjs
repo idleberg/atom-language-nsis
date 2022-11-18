7781c8610be8f1f2f1d56b4082f109c89ecdcaa8
Jan T. Sott <git@idleberg.com>
2022-11-18 10:07:31 +0100

    remove terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
-
-const production = !process.env.ROLLUP_WATCH;
 
 const plugins = [
   commonjs(),
@@ -12,7 +9,6 @@ const plugins = [
   nodeResolve({
     preferBuiltins: true
   }),
-  production && terser(),
   typescript()
 ];
 
