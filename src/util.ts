8be2533ccb8e58bef7ad989908d851fec7cc66aa
Jan T. Sott <git@idleberg.com>
2026-03-07 18:55:21 +0100

    chore: update open

--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,7 @@ import { constants, promises as fs } from 'node:fs';
 import { platform } from 'node:os';
 import { resolve } from 'node:path';
 import { satisfyDependencies } from 'atom-satisfy-dependencies';
+import open from 'open';
 import which from 'which';
 import { name } from '../package.json';
 import Config from './config';
@@ -203,7 +204,6 @@ export function notifyOnCompletion(params: NotificationParams): void {
 }
 
 export async function openURL(nsisCommand: string): Promise<void> {
-	const open = (await import('open')).default;
 	open(
 		`https://idleberg.github.io/NSIS.docset/Contents/Resources/Documents/html/Commands/${nsisCommand}.html?utm_source=atom&utm_content=reference`,
 	);
